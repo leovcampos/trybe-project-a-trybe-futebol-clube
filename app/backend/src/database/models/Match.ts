3ee4b52aab5288c24a334ba598ec4bd92c8d16bb
leovcampos <leonardoviniciuscampos@gmail.com>
2022-11-23 12:08:56 -0300

    models e migrations progress

---
+++ app/backend/src/database/models/Match.ts
@@ -0,0 +1,69 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+// import OtherModel from './OtherModel';
+
+class Match extends Model {
+  // declare <campo>: <tipo>;
+}
+
+Match.init({
+  // ... Campos
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeam: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'home_team',
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'home_team_goals',
+  },
+  awayTeam: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'away_team',
+    references: {
+      model: 'teams',
+      key: 'id',
+    },
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'away_team_goals',
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    field: 'in_progress',
+    allowNull: false,
+  },
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  // modelName: 'example',
+  tableName: 'matches',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+// OtherModel.belongsTo(Example, { foreignKey: 'campoA', as: 'campoEstrangeiroA' });
+// OtherModel.belongsTo(Example, { foreignKey: 'campoB', as: 'campoEstrangeiroB' });
+
+// Example.hasMany(OtherModel, { foreignKey: 'campoC', as: 'campoEstrangeiroC' });
+// Example.hasMany(OtherModel, { foreignKey: 'campoD', as: 'campoEstrangeiroD' });
+
+export default Match;
---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,52 @@
+import { Model, DataTypes } from 'sequelize';
+import db from '.';
+import Match from './Match';
+
+class User extends Model {
+  // declare <campo>: <tipo>;
+}
+
+User.init({
+  // ... Campos
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+}, {
+  // ... Outras configs
+  underscored: true,
+  sequelize: db,
+  // modelName: 'example',
+  tableName: 'users',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+Match.belongsTo(User, { foreignKey: 'homeTeam', as: 'home_team' });
+Match.belongsTo(User, { foreignKey: 'awayTeam', as: 'away_team' });
+
+User.hasMany(Match, { foreignKey: 'homeTeam', as: 'home_team' });
+User.hasMany(Match, { foreignKey: 'awayTeam', as: 'away_team' });
+
+export default User;
