a059f879c575b1fd9e3fb50bd03ee889c5d3cc78
leovcampos <leonardoviniciuscampos@gmail.com>
2022-11-24 14:50:30 -0300

    starting routes

---
+++ app/backend/src/routes/login.router.ts
@@ -0,0 +1,8 @@
+import { Router, Request, Response } from 'express';
+import loginController from '../controllers/login.controller';
+
+const router = Router();
+
+router.post('/', (req: Request, res: Response) => {
+  loginController.login(req, res);
+});
