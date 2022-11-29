5b7eea57d642d392d10ba2a2c3c5803d2d33ef64
leovcampos <leonardoviniciuscampos@gmail.com>
2022-11-29 12:41:13 -0300

    tests login

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -82,4 +82,81 @@ describe('Tests about login rote', () => {
     expect(chaiHttpResponse.body).to.haveOwnProperty('message');
     expect(chaiHttpResponse.body.message).to.be.eq('All fields must be filled');
   })
+
+  it('Expect status 400 when req without password', async () => {
+    const invalidReq = {
+      email: 'email@email.com'
+    }
+
+    chaiHttpResponse = await chai
+    .request(app)
+    .post('/login')
+    .send({ ...invalidReq })
+
+    expect(chaiHttpResponse).to.have.status(400);
+    expect(chaiHttpResponse.body).to.haveOwnProperty('message');
+    expect(chaiHttpResponse.body.message).to.be.eq('All fields must be filled');
+  })
+
+  it('Test if user exist', async () => {
+    (User.findOne as sinon.SinonStub).restore();
+    sinon.stub(User, 'findOne').resolves(null);
+
+    const user = {
+      email: 'email@email.com',
+      password: 'password'
+    }
+
+    chaiHttpResponse = await chai
+    .request(app)
+    .post('/login')
+    .send({ ...user })
+
+    expect(chaiHttpResponse).to.have.status(401);
+    expect(chaiHttpResponse.body).to.haveOwnProperty('message');
+    expect(chaiHttpResponse.body.message).to.be.eq('Incorrect email or password');
+  })
+
+  it('Test invalid token', async () => {
+    chaiHttpResponse = await chai
+    .request(app)
+    .post('/login/validate')
+
+    expect(chaiHttpResponse).to.have.status(401);
+    expect(chaiHttpResponse.body).to.haveOwnProperty('message');
+    expect(chaiHttpResponse.body.message).to.be.eq('token not provided');
+  })
+
+  it('Test invalid password', async () => {
+    const user = {
+      email: "email@email.com",
+      password: "password_incorrect"
+    };
+
+    chaiHttpResponse = await chai
+    .request(app)
+    .post('/login')
+    .send({ ...user })
+
+    expect(chaiHttpResponse).to.have.status(401);
+    expect(chaiHttpResponse.body).to.haveOwnProperty('message');
+    expect(chaiHttpResponse.body.message).to.be.eq('Incorrect email or password');
+  })
+
+  it('Test login sucess', async () => {
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
+    sinon.stub(User, 'findByPk').resolves({ role: 'admin' } as User);
+
+    chaiHttpResponse = await chai
+    .request(app)
+    .get('/login/validate')
+    .set('authorization', token)
+
+    expect(chaiHttpResponse).to.have.status(200);
+    expect(chaiHttpResponse.body).to.haveOwnProperty('role');
+    expect(chaiHttpResponse.body.role).to.be.eq('admin');
+    
+    (User.findByPk as sinon.SinonStub).restore();
+  })
 });
